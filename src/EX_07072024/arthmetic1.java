44293e52bab4660892d23de4a4c3a15a30163f7c
Ram <ram.king0213@gmail.com>
2024-07-13 09:33:42 +0530

    All Files

--- src/EX_07072024/arthmetic1.java
+++ src/EX_07072024/arthmetic1.java
@@ -5,7 +5,9 @@ public class arthmetic1 {
     public static void main(String[] args) {
         System.out.println("Hello Team");
         System.out.println(1+2);
-        System.out.println(1/0);
+       // System.out.println(1/0);
+        System.out.println(10*3);
+        System.out.println(15/6);
 
     }
 
