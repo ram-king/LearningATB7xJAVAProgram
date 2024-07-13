035e9d71471d4977b5fd2cc54568883d50d5e3e9
Ram <ram.king0213@gmail.com>
2024-07-13 09:42:25 +0530

    All Files

--- src/Invalidsyntaxprogram.java
+++ src/Invalidsyntaxprogram.java
@@ -1,7 +1,7 @@
 public class Invalidsyntaxprogram {
 //    public static void main(String args[]) {
   //      System.out.println("Not Working");
-    //}
+    //} throws an error due to invalid syntax
 
     public static void main(String[] args) {
         System.out.println("Works from 6th line");
