8398b86a8073ee87a56a00d3aae3ed705d22a91c
Ram <ram.king0213@gmail.com>
2024-07-13 09:17:33 +0530

    All Files

---
+++ src/Invalidsyntaxprogram.java
@@ -0,0 +1,9 @@
+public class Lab002 {
+    public static void main(String args[]) {
+        System.out.println("Not Working");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Works from 6th line");
+    }
+}
