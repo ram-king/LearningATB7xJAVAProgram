2f73266abe4a809cb23eb406e5e3dce4a7c7d20d
Ram <ram.king0213@gmail.com>
2024-07-13 09:30:12 +0530

    All Files

---
+++ src/EX_07072024/arthmetic1.java
@@ -0,0 +1,12 @@
+package EX_07072024;
+
+
+public class arthmetic1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Team");
+        System.out.println(1+2);
+        System.out.println(1/0);
+
+    }
+
+}
--- src/Invalidsyntaxprogram.java
+++ src/Invalidsyntaxprogram.java
@@ -1,7 +1,7 @@
-public class Lab002 {
-    public static void main(String args[]) {
-        System.out.println("Not Working");
-    }
+public class Invalidsyntaxprogram {
+//    public static void main(String args[]) {
+  //      System.out.println("Not Working");
+    //}
 
     public static void main(String[] args) {
         System.out.println("Works from 6th line");
