494e6b31707fe8e53e3b69a30705a14bc5d71a24
Ram <ram.king0213@gmail.com>
2024-07-28 12:28:59 +0530

    All Files

---
+++ src/EX_20072024/Cond1.java
@@ -0,0 +1,13 @@
+package EX_20072024;
+
+public class Cond1 {
+    public static void main(String[] args) {
+        float a2 = 10f;
+        a2 = a2%3; //Modulus means remainder of division
+        if (a2 == 2){
+            System.out.println("O/p not available");
+        }else {
+            System.out.println("o/p ->"+a2);
+        }
+    }
+}
--- src/EX_20072024/Conditions.java
+++ src/EX_20072024/Conditions.java
@@ -33,5 +33,6 @@ public class Conditions {
         }
         System.out.println(a1);
 
+
     }
 }
