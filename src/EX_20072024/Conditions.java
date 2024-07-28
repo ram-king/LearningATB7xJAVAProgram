e091e0d7389c6b595656fe34d3f8018a32f75b57
Ram <ram.king0213@gmail.com>
2024-07-28 12:00:02 +0530

    All Files

---
+++ src/EX_20072024/Conditions.java
@@ -0,0 +1,37 @@
+package EX_20072024;
+
+public class Conditions {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a!=2){
+            System.out.println("True");
+        }else {
+            System.out.println("false");
+        }
+        if (a>=2){
+            System.out.println("ture");
+        }
+        else {
+            System.out.println("false");
+        }
+        // Two Different Conditions
+        if(a==10 && a>=2){
+            System.out.println("True");
+        }
+
+        if (2+3 == 6){
+            System.out.println("fool"); // here there is no relation with 'a' as we're not calling it
+        }else {
+            System.out.println("clever");
+        }
+        boolean a1 = true;
+        a1 = !a1;
+        if (a1==a1){
+            System.out.println("Good");
+        }else {
+            System.out.println("check again");
+        }
+        System.out.println(a1);
+
+    }
+}
--- src/EX_21072024/LeftSideTriangle.java
+++ src/EX_21072024/LeftSideTriangle.java
@@ -11,11 +11,21 @@ public class LeftSideTriangle {
 
             symbol = symbol+"\b";
 
-        }
-        //System.out.println(symbol + "\b");
-
-        }
-    }
+        }}}
+//        String symbol = " ";
+//        for (int i = 5; i >=1 ; i--) {
+//            for (int j = 1; j <=i; j++) {
+//
+//                System.out.print("*");
+//
+//            }
+//
+//            System.out.println(symbol);
+//
+//        }
+//
+//        }
+//    }
 
 
 
