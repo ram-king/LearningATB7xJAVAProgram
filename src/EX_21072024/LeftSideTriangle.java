33d0e7067ca36d473c7876902b87b7588918b8dc
Ram <ram.king0213@gmail.com>
2024-08-03 14:12:50 +0530

    All Files

--- src/EX_21072024/LeftSideTriangle.java
+++ src/EX_21072024/LeftSideTriangle.java
@@ -3,30 +3,25 @@ package EX_21072024;
 public class LeftSideTriangle {
     public static void main(String[] args) {
 
-        String symbol = "*****";
-
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println(symbol);
-
-            symbol = symbol+"\b";
-
-        }}}
-//        String symbol = " ";
-//        for (int i = 5; i >=1 ; i--) {
-//            for (int j = 1; j <=i; j++) {
+//        String symbol = "*****";
 //
-//                System.out.print("*");
-//
-//            }
+//        for (int i = 1; i <= 5; i++) {
 //
 //            System.out.println(symbol);
 //
-//        }
+//            symbol = symbol+"\b";
 //
 //        }
-//    }
+//        }
+//        }
+        String symbol = " ";
+        for (int i = 5; i >=1 ; i--) {
+            for (int j = 1; j <= i; j++) {
 
+                System.out.print("*");
 
+            }
 
+            System.out.println(symbol);
 
+        }}}
