2407aa72c8ac9a8720c0ee2e7aaf91bf7e16d187
Ram <ram.king0213@gmail.com>
2024-08-03 15:13:33 +0530

    All Files

---
+++ src/EX_21072024/Lab4.java
@@ -0,0 +1,13 @@
+package EX_21072024;
+
+public class Lab4 {
+    public static void main(String[] args) {
+        int age  = 33;
+        int ram1 = 33;
+        String name = "Ram";
+
+        for (int i = 1; i < 6; i++) { // 1 to 5
+            System.out.println(i); /// 5<5
+        }
+    }
+}
