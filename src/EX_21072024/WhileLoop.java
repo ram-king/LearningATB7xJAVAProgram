c1dbda415a60f0d6703a5584a78ca6b8298eaaa7
Ram <ram.king0213@gmail.com>
2024-08-11 10:02:24 +0530

    All Files

---
+++ src/EX_21072024/WhileLoop.java
@@ -0,0 +1,12 @@
+package EX_21072024;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int a = 10;
+        while (a>=6){
+            System.out.println(a);
+            a--;
+        }
+        System.out.println(++a);
+    }
+}
