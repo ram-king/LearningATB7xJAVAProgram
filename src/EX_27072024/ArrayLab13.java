1cea34e72be0cf015923a6abbbbce81eefee7b47
Ram <ram.king0213@gmail.com>
2024-08-17 17:01:04 +0530

    All Files

--- src/EX_27072024/ArrayLab13.java
+++ src/EX_27072024/ArrayLab13.java
@@ -2,15 +2,17 @@ package EX_27072024;
 
 public class ArrayLab13 {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5, 4, 3, 2, 1};
+        int[] numbers = {10, 2, 32, 48, 25, 34, 13, 29, 61};
         int max = 2;
-        int min = 2;
+        int min = numbers[0];
+        //Maximum number in the array
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] > max) {
                 max = numbers[i];
             }
         }
         System.out.println(max);
+        //Minimum number in the array
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] < min) {
                 min = numbers[i];
