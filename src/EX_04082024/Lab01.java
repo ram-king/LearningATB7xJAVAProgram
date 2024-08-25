1ce6cb2a547d6e06484d2ff42a0107cb0e1e9d8b
Ram <ram.king0213@gmail.com>
2024-08-25 11:28:08 +0530

    All Files

---
+++ src/EX_04082024/Lab01.java
@@ -0,0 +1,24 @@
+package EX_04082024;
+
+import java.sql.SQLOutput;
+
+public class Lab01 {
+    public static void main(String[] args) {
+        // Constructors
+        // 1. default - 70%
+        // 2. Parameters - 25%
+        // 3. Copy Constructors  - ~ 1-4%
+        // this vs super - why? they are used?
+        // Static keyword and static methods
+
+
+        // Constructors
+        // 1. default - 70%
+        // 2. Parameters - 25%
+        // 3. Copy Constructors  - ~ 1-4%
+
+        Lab01 lab01=new Lab01();
+        System.out.println(lab01);
+
+    }
+}
---
+++ src/EX_25082024/Lab001.java
@@ -0,0 +1,13 @@
+package EX_25082024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        // API Testing
+
+        // URL - https://abc.com/xyz/?q=1#anchhr=09
+        //
+        //Which one is path param - xyz
+        //Which one is query param - #anchhr=09
+
+    }
+}
